026f78900be556f25b4b266198f31a8d84097e3b
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-11-28 00:30:31 +0900

    Nov28

---
+++ Assets/02.Script/Card.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer card;
+    [SerializeField] SpriteRenderer charactor;
+    [SerializeField] TMP_Text nameTMP;
+    [SerializeField] TMP_Text attackTMP;
+    [SerializeField] TMP_Text healthTMP;
+    [SerializeField] Sprite cardFront;
+    [SerializeField] Sprite cardBack;
+
+    public Item item;
+    bool isFront;
+    
+    public void Setup(Item item, bool isFront)
+    {
+        this.item = item;
+        this.isFront = isFront;
+
+        if (this.isFront)
+        {
+            charactor.sprite = this.item.sprite;
+            nameTMP.text = this.item.name;
+            attackTMP.text = this.item.attack.ToString();
+            healthTMP.text = this.item.health.ToString();
+        }
+
+        else
+        {
+            card.sprite = cardBack;
+            nameTMP.text = "";
+            attackTMP.text = "";
+            healthTMP.text = "";
+        }
+    }
+}
---
+++ Assets/02.Script/Utils.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    // static으로 CardManager에 할당
+    public static Quaternion QI => Quaternion.identity;
+}
