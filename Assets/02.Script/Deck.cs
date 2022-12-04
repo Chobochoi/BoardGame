c469f8e07e6f12760c0a2de4683e5055e098afcd
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-12-05 02:30:15 +0900

    Dec5 IntroGameImage Setting

---
+++ Assets/02.Script/Deck.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public Sprite[] cardSprites;
+    int[] cardVaules = new int[10];
+    
+    void Start()
+    {
+        
+    }
+
+    void GetCardValues()
+    {
+        int num = 0;
+    }
+}
---
+++ Assets/02.Script/DeckManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckManager : MonoBehaviour
+{
+    [SerializeField] public Button plusCardBtn;
+    [SerializeField] public Button coin1Btn;
+    [SerializeField] public Button coin2Btn;
+    [SerializeField] public Button coin3Btn;
+    [SerializeField] public Button coin4Btn;
+    [SerializeField] public Button coin5Btn;
+    [SerializeField] public Button allInBtn;
+    [SerializeField] public Button giveUpBtn;
+    
+    void Start()
+    {
+        plusCardBtn.onClick.AddListener(() => plusClick());
+        coin1Btn.onClick.AddListener(() => coinClick());
+        coin2Btn.onClick.AddListener(() => coinClick());
+        coin3Btn.onClick.AddListener(() => coinClick());
+        coin4Btn.onClick.AddListener(() => coinClick());
+        coin5Btn.onClick.AddListener(() => coinClick());
+        allInBtn.onClick.AddListener(() => coinClick());
+        giveUpBtn.onClick.AddListener(() => giveUpClick());
+    }
+
+    void plusClick()
+    {
+        
+    }
+
+    void coinClick()
+    {
+
+    }
+
+    void giveUpClick()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
