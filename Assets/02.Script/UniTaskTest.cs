29aea8d19f06235af22e641fc20babc39432443d
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2023-01-25 23:20:23 +0900

    2023 Jan 25

--- Assets/02.Script/UniTaskTest.cs
+++ Assets/02.Script/UniTaskTest.cs
@@ -10,46 +10,47 @@ using System.Threading.Tasks;
 
 public class UniTaskTest : MonoBehaviour
 {
-    private IEnumerator CoTest()
-    {
-        yield return new WaitForSeconds(1f);
-        Debug.Log("Co 1초지남");
-    }
-
-    private async UniTaskVoid TaskTest()
-    {
-        //DelayType.UnscaledDeltaTime Timescale 값이 0이 되어도 계속 진행합니다.
-        await UniTask.Delay(TimeSpan.FromSeconds(1f), DelayType.UnscaledDeltaTime);
-        Debug.Log("Task 1초지남");
-    }
-
-    private void Start()
-    {
-        Time.timeScale = 0;
-        StartCoroutine(CoTest());
-
-        // Void를 사용하면 Forget을 붙이라고 문서에 나와있습니다.
-        TaskTest().Forget();
-    }
-
-    //public int count;
-
     //private IEnumerator CoTest()
     //{
-    //    yield return new WaitUntil(() => count == 3);
-    //    Debug.Log("Co 3이 되었다.");        
+    //    yield return new WaitForSeconds(1f);
+    //    Debug.Log("Co 1초지남");
     //}
 
     //private async UniTaskVoid TaskTest()
     //{
-    //    await UniTask.WaitUntil(() => count == 3);
-    //    Debug.Log("Task 3이 되었다.");
+    //    //DelayType.UnscaledDeltaTime Timescale 값이 0이 되어도 계속 진행합니다.
+    //    await UniTask.Delay(TimeSpan.FromSeconds(1f), DelayType.UnscaledDeltaTime);
+    //    Debug.Log("Task 1초지남");
     //}
 
-    //private void Start()    {      
+    //private void Start()
+    //{
+    //    Time.timeScale = 0;
+    //    StartCoroutine(CoTest());
 
+    //    // Void를 사용하면 Forget을 붙이라고 문서에 나와있습니다.
     //    TaskTest().Forget();
     //}
+
+    public int count;
+
+    private IEnumerator CoTest()
+    {
+        yield return new WaitUntil(() => count == 3);
+        Debug.Log("Co 3이 되었다.");
+    }
+
+    private async UniTaskVoid TaskTest()
+    {
+        await UniTask.WaitUntil(() => count == 3);
+        Debug.Log("Task 3이 되었다.");
+    }
+
+    private void Start()
+    {
+
+        TaskTest().Forget();
+    }
 
     // 인터넷 이미지 가져오기
 
